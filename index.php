1642495a6d113c900b631af5a96935c1cf96c05d
myzayy <zaetsmaks06@gmail.com>
2025-03-03 23:32:55 +0100

    Create index.php and some code

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja prva stranka v php</title>
+</head>
+<body>
+    <h1>Vitajte na mojej prvej stranke v php!</h1>
+    <?php
+    echo "<h1>Hello World!</h1>";
+    echo "Dnes je " . date(format:  "d.m.Y") . "<br>";
+    echo "Aktualny cas je " . date(format:"H:i:s") . "<br>";
+    ?>
+</body>
+</html>
