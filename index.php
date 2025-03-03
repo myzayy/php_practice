f48039f97398f47988c2ba17e6c002d147492bb1
myzayy <zaetsmaks06@gmail.com>
2025-03-03 23:35:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
     <title>Moja prva stranka v php</title>
 </head>
 <body>
+
     <h1>Vitajte na mojej prvej stranke v php!</h1>
     <?php
     echo "<h1>Hello World!</h1>";
     echo "Dnes je " . date(format:  "d.m.Y") . "<br>";
     echo "Aktualny cas je " . date(format:"H:i:s") . "<br>";
     ?>
+    
 </body>
 </html>
